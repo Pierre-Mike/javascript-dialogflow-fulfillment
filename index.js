d6c4dc2c7df0e9b49ffeba4e14ab58e1bf590b35
pierre-mike <pm.lemeliner@gmail.com>
2019-05-31 12:42:07 -0700

    add calendar google and connection sharepoint

--- index.js
+++ index.js
@@ -1,37 +1,165 @@
 "use strict";
 
-const { WebhookClient } = require("dialogflow-fulfillment");
+const { WebhookClient, Card } = require("dialogflow-fulfillment");
 const express = require("express");
 const bodyParser = require("body-parser");
 const basicAuth = require("express-basic-auth");
+const { google } = require("googleapis");
 
+const CONFIG = process.env.CONFIG || require("./settings.json"); 
+const serviceAccountAuth = new google.auth.JWT({
+  email: CONFIG.google.client_email,
+  key: CONFIG.google.private_key,
+  scopes: "https://www.googleapis.com/auth/calendar"
+});
+const calendarId = CONFIG.google.calendarId; // looks like "6ujc6j6rgfk02cp02vg6h38cs0@group.calendar.google.com"
+
+const calendar = google.calendar("v3");
+const timeZone = "America/Los_Angeles";
+const timeZoneOffset = "-07:00";
+
+/* 
 app.use(
   basicAuth({
     users: { admin: "supersecret" }
   })
-);
+);s
+ */
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+function check_appointment(agent) {
+  const dateTimeStart = new Date(
+    Date.parse(
+      agent.parameters.date.split("T")[0] +
+        "T" +
+        agent.parameters.time.split("T")[1].split("-")[0] +
+        timeZoneOffset
+    )
+  );
+  const dateTimeEnd = new Date(
+    new Date(dateTimeStart).setHours(dateTimeStart.getHours() + 1)
+  );
+  const appointmentTimeString = dateTimeStart.toLocaleString("en-US", {
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    timeZone: timeZone
+  });
+
+  // Check the availibility of the time, and make an appointment if there is time on the calendar
+  return createCalendarEvent(dateTimeStart, dateTimeEnd)
+    .then(() => {
+      agent.add(
+        `Ok, let me see if we can fit you in. ${appointmentTimeString} is fine!?`
+      );
+    })
+    .catch(() => {
+      agent.add(
+        `I'm sorry, there are no slots available for ${appointmentTimeString}.`
+      );
+    });
+}
+
+function createCalendarEvent(dateTimeStart, dateTimeEnd) {
+  return new Promise((resolve, reject) => {
+    calendar.events.list(
+      {
+        auth: serviceAccountAuth, // List events for time period
+        calendarId: calendarId,
+        timeMin: dateTimeStart.toISOString(),
+        timeMax: dateTimeEnd.toISOString()
+      },
+      (err, calendarResponse) => {
+        // Check if there is a event already on the Bike Shop Calendar
+        if (err || calendarResponse.data.items.length > 0) {
+          reject(
+            err ||
+              new Error("Requested time conflicts with another appointment")
+          );
+        } else {
+          // Create event for the requested time period
+          calendar.events.insert(
+            {
+              auth: serviceAccountAuth,
+              calendarId: calendarId,
+              resource: {
+                summary: "Bike Appointment",
+                start: { dateTime: dateTimeStart },
+                end: { dateTime: dateTimeEnd }
+              }
+            },
+            (err, event) => {
+              err ? reject(err) : resolve(event);
+            }
+          );
+        }
+      }
+    );
+  });
+}
 
 function welcome(agent) {
   agent.add(`Welcome to Express.JS webhook!`);
 }
 
+function connection(agent) {
+  const credentials = {
+    client: {
+      id: CONFIG.microsoft.clientId,
+      secret: CONFIG.microsoft.secret
+    },
+    auth: {
+      tokenHost: CONFIG.microsoft.loginUrl,
+      tokenPath: "/" + CONFIG.microsoft.tenant + CONFIG.microsoft.tokenPath,
+      authorizePath:
+        "/" + CONFIG.microsoft.tenant + CONFIG.microsoft.authorizePath
+    }
+  };
+  const oauth2 = require("simple-oauth2").create(credentials);
+
+  const getURLAuthorizarionPlusRessource = state => {
+    const redirect_uri = CONFIG.microsoft.redirect_uri;
+
+    var urlAutorization = oauth2.authorizationCode.authorizeURL({
+      redirect_uri,
+      state
+    });
+    return urlAutorization + "&resource=https://graph.microsoft.com/";
+  };
+  agent.add(
+    new Card({
+      title: "Title: this is a card title",
+      imageUrl:
+        "http://www.tascmanagement.com/wp-content/uploads/2016/05/course-logo-small-SP-300x250.png",
+      text:
+        "This is the body text of a card.  You can even use line\n  breaks and emoji! 💁",
+      buttonText: "Login",
+      buttonUrl: getURLAuthorizarionPlusRessource("test")
+    })
+  );
+}
 function fallback(agent) {
   agent.add(`I didn't understand`);
   agent.add(`I'm sorry, can you try again?`);
 }
 
-function WebhookProcessing(req, res) {
-  const agent = new WebhookClient({ request: req, response: res });
-  console.info(`agent set`);
+function getMapIntents() {
   let intentMap = new Map();
+  intentMap.set("Check an appointment", check_appointment);
   intentMap.set("Default Welcome Intent", welcome);
-  intentMap.set("Default Fallback Intent", fallback);
-  // intentMap.set('<INTENT_NAME_HERE>', yourFunctionHandler);
-  agent.handleRequest(intentMap);
+  intentMap.set("Login SharePoint", connection);
+  intentMap.set("Fallback", fallback);
+  return intentMap;
+}
+
+function WebhookProcessing(request, response) {
+  const agent = new WebhookClient({ request, response });
+  let mapIntents = getMapIntents();
+  agent.handleRequest(mapIntents);
 }
 
 // Webhook
@@ -44,6 +172,8 @@ app.get("/", function(req, res) {
   res.send(JSON.stringify({ Hello: `word` }));
 });
 
-app.listen(process.env.PORT || 80, function() {
+app.get("/getAToken", function(req, res) {});
+
+app.listen(process.env.PORT || 8080, function() {
   console.info(`Webhook listening on port 8080!`);
 });
